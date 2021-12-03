eeff922b27e54ed01f50be92ab60a35029b40009
John Hunter Kohler <jhunterkohler@gmail.com>
2021-12-03 14:59:05 -0500

    add threads files and spinlock

---
+++ hlibc/threads.c
@@ -0,0 +1,49 @@
+#include <hlibc/threads.h>
+
+int nanosleep_full(long nsec, time_t sec)
+{
+    int ret = 0;
+    struct timespec spec = { .tv_nsec = nsec, .tv_sec = sec };
+    do {
+        ret = nanosleep(&spec, &spec);
+    } while (ret && errno == EINTR);
+    return ret;
+}
+
+int spinlock_lock(struct spinlock *lock)
+{
+    while (true) {
+        for (int i = 0; i < 100000; i++) {
+            if (atomic_flag_test_and_set(&lock->flag)) {
+                return 0;
+            }
+        }
+        sched_yield();
+    }
+}
+
+int spinlock_trylock(struct spinlock *lock)
+{
+    if (atomic_flag_test_and_set(&lock->flag))
+        return 0;
+    else
+        return EBUSY;
+}
+
+int spinlock_unlock(struct spinlock *lock)
+{
+    atomic_flag_clear(lock->flag);
+    return 0;
+}
+
+int spinlock_init(struct spinlock *lock)
+{
+    atomic_flag_clear(lock->flag);
+    return 0;
+}
+
+int spinlock_destroy(struct spinlock *lock)
+{
+    atomic_flag_clear(lock->flag);
+    return 0;
+}
---
+++ hlibc/threads.h
@@ -0,0 +1,42 @@
+#ifndef HLIBC_THREADS_H_
+#define HLIBC_THREADS_H_
+
+#include <stdatomic.h>
+#include <time.h>
+#include <sched.h>
+#include <pthread.h>
+#include <hlibc/def.h>
+
+/*
+ * Sleeps the full amount of time specified as a summed duration, while
+ * ignoring any signal interupts.
+ */
+int nanosleep_full(long nsec, time_t sec);
+
+/*
+ * Standard spinlock. Requires c11 atomics.
+ */
+struct spinlock {
+    atomic_flag flag;
+};
+
+/*
+ * Static `struct spinlock` initializer.
+ */
+#define SPINLOCK_INIT ((struct spinlock){ .flag = ATOMIC_FLAG_INIT })
+
+int spinlock_lock(struct spinlock *lock);
+int spinlock_trylock(struct spinlock *lock);
+int spinlock_unlock(struct spinlock *lock);
+
+/*
+ * Dynamic `struct spinlock` initializer.
+ */
+int spinlock_init(struct spinlock *lock);
+
+/*
+ * Dynamic `struct spinlock` destructor.
+ */
+int spinlock_destroy(struct spinlock *lock);
+
+#endif
