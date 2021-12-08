8445d07ba2cda1e31507bba0633c7e419b1ff13b
John Hunter Kohler <jhunterkohler@gmail.com>
2021-12-08 04:02:30 -0500

    added regmatch_text

--- hlibc/regex.c
+++ hlibc/regex.c
@@ -1,6 +1,34 @@
+/*
+ * Copyright (C) 2021 Hunter Kohler <jhunterkohler@gmail.com>
+ */
+#include <hlibc/string.h>
 #include <hlibc/regex.h>
+
+static inline bool regmatch_valid(const regmatch_t *match)
+{
+    return match->rm_so >= 0 && match->rm_so <= match->rm_eo;
+}
 
 size_t regmatch_len(const regmatch_t *match)
 {
-    return match->rm_eo - match->rm_so;
+    return regmatch_valid(match) ? 0 : match->rm_eo - match->rm_so;
+}
+
+int regmatch_text(const char *restrict str, const regmatch_t *match,
+                  char *restrict *dest)
+{
+    if (regmatch_valid(match)) {
+        size_t len = regmatch_len(match);
+        char *buf = memdup(str + match->rm_so, len + 1);
+
+        if (!buf)
+            return ENOMEM;
+
+        buf[len] = 0;
+        *dest = buf;
+        return 0;
+    } else {
+        *dest = NULL;
+        return 0;
+    }
 }
--- hlibc/regex.h
+++ hlibc/regex.h
@@ -12,4 +12,11 @@
 
 size_t regmatch_len(const regmatch_t *match);
 
+/*
+ * Allocated a new buffer at `dest`, and copies the regex match `match` from
+ * string. `dest` is set to `NULL` for non-matches.
+ */
+int regmatch_text(const char *restrict str, const regmatch_t *match,
+                  char *restrict *dest);
+
 #endif
