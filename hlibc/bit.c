d6eb5dea74edf282dce222e5407d33ccc675ec82
John Hunter Kohler <jhunterkohler@gmail.com>
2021-12-15 00:44:01 -0500

    refix bit rotation namings

--- hlibc/bit.c
+++ hlibc/bit.c
@@ -14,7 +14,7 @@ uint8_t rotl_8(uint8_t x, size_t n)
     return (x << n) | (x >> (8 - n));
 }
 
-uint8_t rotr8(uint8_t x, size_t n)
+uint8_t rotr_8(uint8_t x, size_t n)
 {
     return (x >> n) | (x << (8 - n));
 }
@@ -24,7 +24,7 @@ uint16_t rotl_16(uint16_t x, size_t n)
     return (x << n) | (x >> (16 - n));
 }
 
-uint16_t rotr16(uint16_t x, size_t n)
+uint16_t rotr_16(uint16_t x, size_t n)
 {
     return (x >> n) | (x << (16 - n));
 }
@@ -34,7 +34,7 @@ uint32_t rotl_32(uint32_t x, size_t n)
     return (x << n) | (x >> (32 - n));
 }
 
-uint32_t rotr32(uint32_t x, size_t n)
+uint32_t rotr_32(uint32_t x, size_t n)
 {
     return (x >> n) | (x << (32 - n));
 }
@@ -44,7 +44,7 @@ uint64_t rotl_64(uint64_t x, size_t n)
     return (x << n) | (x >> (64 - n));
 }
 
-uint64_t rotr64(uint64_t x, size_t n)
+uint64_t rotr_64(uint64_t x, size_t n)
 {
     return (x >> n) | (x << (64 - n));
 }
@@ -54,7 +54,7 @@ uint128_t rotl_128(uint128_t x, size_t n)
     return (x << n) | (x >> (128 - n));
 }
 
-uint128_t rotr128(uint128_t x, size_t n)
+uint128_t rotr_128(uint128_t x, size_t n)
 {
     return (x >> n) | (x << (128 - n));
 }
