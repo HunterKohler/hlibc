44e3f974416a04a7eb14381dda21ca3949ffb2b2
John Hunter Kohler <jhunterkohler@gmail.com>
2022-02-20 10:59:58 -0500

    update ctype inline

--- include/hlibc/ctype.h
+++ include/hlibc/ctype.h
@@ -3,37 +3,37 @@
 
 #include <hlibc/def.h>
 
-static inline bool hlib_isalpha(char c)
+static inline bool isalpha(char c)
 {
     extern const bool isalpha_table[256];
     return isalpha_table[(unsigned char)c];
 }
 
-static inline bool hlib_isalnum(char c)
+static inline bool isalnum(char c)
 {
     extern const bool isalnum_table[256];
     return isalnum_table[(unsigned char)c];
 }
 
-static inline bool hlib_isdigit(char c)
+static inline bool isdigit(char c)
 {
     extern const bool isdigit_table[256];
     return isdigit_table[(unsigned char)c];
 }
 
-static inline bool hlib_isxdigit(char c)
+static inline bool isxdigit(char c)
 {
     extern const bool isxdigit_table[256];
     return isxdigit_table[(unsigned char)c];
 }
 
-static inline bool hlib_toupper(char c)
+static inline bool toupper(char c)
 {
     extern const char toupper_table[256];
     return toupper_table[(unsigned char)c];
 }
 
-static inline bool hlib_tolower(char c)
+static inline bool tolower(char c)
 {
     extern const char tolower_table[256];
     return tolower_table[(unsigned char)c];
